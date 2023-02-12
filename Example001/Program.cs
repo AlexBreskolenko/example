c52be6fddd071faf5f2d558816e5061f6ffb6ead
Alex <abreskolenko@yandex.ru>
2023-02-12 19:17:38 +0300

    add example 001

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.Clear();
+
+int Max (int []arr)
+{
+    int maxNumber = arr[0];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (maxNumber < arr[i])
+        {
+            maxNumber = arr[i];
+        }
+    }
+
+    return maxNumber;
+}
+
+int []array = {5, 88, 13, 67, 55, 4, 0, -3};
+
+Console.Write("Max number = " + Max(array));
