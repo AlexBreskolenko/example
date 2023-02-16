c011c299c77ba0bd8dc7e319219fd234d2518f49
Alex <abreskolenko@yandex.ru>
2023-02-16 18:17:45 +0300

    Create example 008

---
+++ Example008/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет
+*/
+
+Console.Clear();
+
+Console.Write("Enter the first number : ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number : ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if(firstNumber * firstNumber == secondNumber || secondNumber * secondNumber == firstNumber)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
