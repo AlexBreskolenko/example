e2d58a6b516c97fc6ff93d046c0e2f8e5f11df1a
Alex <abreskolenko@yandex.ru>
2023-02-16 11:18:13 +0300

    Add comentaries

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,5 +1,8 @@
-﻿Console.Clear();
+﻿//Создаем массив и ищем максимальный элемент в нем
 
+Console.Clear();
+
+//Создаем функцию принимает массив ,возвращает целое число
 int Max (int []arr)
 {
     int maxNumber = arr[0];
@@ -15,6 +18,6 @@ int Max (int []arr)
     return maxNumber;
 }
 
-int []array = {5, 88, 13, 67, 55, 4, 0, -3};
+int []array = {5, 88, 13, 67, 505, 4, 0, -3};
 
 Console.Write("Max number = " + Max(array));
