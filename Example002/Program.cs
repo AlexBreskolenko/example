fb26f8126ec1e0675093115ceb99d5941f39e811
Alex <abreskolenko@yandex.ru>
2023-02-16 11:58:26 +0300

    Add task.txa and create example 002

---
+++ Example002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+//Создаем массив
+int[] array = {5, 3, 6, 11, 5, 45, 13};
+
+//Длинна массива
+int arraySize = array.Length;
+
+//Просим ввети искомое число
+Console.Write("Введите искомое число : ");
+string str = Console.Read();
+
+//Конвертируем его
+int numberDesired;
+numberDesired = Convert.ToInt32(str);
+
