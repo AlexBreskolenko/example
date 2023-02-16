b52fba7283235a9344a0197eb030e9d3c0e77029
Alex <abreskolenko@yandex.ru>
2023-02-16 12:41:42 +0300

    Create example 002

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -3,14 +3,34 @@
 //Создаем массив
 int[] array = {5, 3, 6, 11, 5, 45, 13};
 
+//Флаг найдено число иль нет
+bool flag = true;
+
 //Длинна массива
 int arraySize = array.Length;
 
 //Просим ввети искомое число
 Console.Write("Введите искомое число : ");
-string str = Console.Read();
+string str = Console.ReadLine();
 
 //Конвертируем его
 int numberDesired;
 numberDesired = Convert.ToInt32(str);
 
+//Проходимся по массиву
+for (int i = 0; i < arraySize; i++)
+{
+    //если нашли его то выводим и прекращаем цикл
+    if (array[i] == numberDesired)
+    {
+        Console.WriteLine(array[i]);
+        flag = false;
+        break;
+    }
+}
+
+//Если не нашли число выведем об этом
+if(flag)
+{
+    Console.Write("Искомого числа нет в массиве !");
+}
